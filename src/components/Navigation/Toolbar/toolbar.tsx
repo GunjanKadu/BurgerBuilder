eeac3f05ef5a13983e0c072acaea15cf5a985a9a
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-01-01 17:04:54 +0100

    Adding Resuable Navigation Component

--- src/components/Navigation/Toolbar/toolbar.tsx
+++ src/components/Navigation/Toolbar/toolbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './toolbar.css'
 import Logo from '../../Logo/logo'
-
+import NavigationItems from '../NavigationItems/Navigationitems'
 const toolBar = (props: any) => (
     <header className="Toolbar">
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
