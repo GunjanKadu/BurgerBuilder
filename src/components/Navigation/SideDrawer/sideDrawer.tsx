59a1d5de65f34277c911fb6283fb690063a3ea17
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-01-02 15:46:04 +0100

    Adding Backdrop and sidedrawer dissappear

--- src/components/Navigation/SideDrawer/sideDrawer.tsx
+++ src/components/Navigation/SideDrawer/sideDrawer.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import Logo from '../../Logo/logo';
 import NavigationItems from '../NavigationItems/Navigationitems';
 import './sideDrawer.css'
+import BackDrop from '../../UI/Backdrop/backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props: any) => {
+    let attachedClasses = ["SideDrawer", "Close"]
+    if (props.open) {
+        attachedClasses = ["SideDrawer", "Open"]
+    }
     return (
-        <div className="SideDrawer">
-            <div className="Logo1">
-                <Logo />
+        <Aux>
+            <BackDrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className="Logo1">
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 }
 
