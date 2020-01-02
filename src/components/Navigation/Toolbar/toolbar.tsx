53f29da3b8c111d6e084ae443ee9ddec00c5cf8b
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-01-02 11:32:25 +0100

    Adding a responsive sidedrawer

--- src/components/Navigation/Toolbar/toolbar.tsx
+++ src/components/Navigation/Toolbar/toolbar.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './toolbar.css'
 import Logo from '../../Logo/logo'
 import NavigationItems from '../NavigationItems/Navigationitems'
+
 const toolBar = (props: any) => (
     <header className="Toolbar">
         <div>MENU</div>
-        <Logo />
+        <div className="Logo2">
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
