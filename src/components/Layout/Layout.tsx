d25f8dbde2678198e2ad33789f4f103baa66066b
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-01-02 15:46:04 +0100

    Adding Backdrop and sidedrawer dissappear

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import './layout.css'
 import ToolBar from "../Navigation/Toolbar/toolbar";
 import SideDrawer from "../Navigation/SideDrawer/sideDrawer";
 
-const layout: React.FC = (props: any) => {
-    return (
-        <Aux>
-            <ToolBar />
-            <SideDrawer />
-            <main className='Content'>
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <ToolBar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className='Content'>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
-export default layout
+export default Layout
