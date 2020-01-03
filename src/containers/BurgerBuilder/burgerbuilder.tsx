24c59a9eebc73fa18bcd2d19b5ff3669898b966b
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-01-03 12:20:25 +0100

    Changing the folder structure

--- src/containers/BurgerBuilder/burgerbuilder.tsx
+++ src/containers/BurgerBuilder/burgerbuilder.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Aux/Aux";
 import Burger from "../../components/Burger/Burger";
 import burgerState from '../../assets/interface/burgetState';
 import BuildControls from '../../components/Burger/BuildControls/buildControls';
