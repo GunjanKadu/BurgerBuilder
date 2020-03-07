46c686184b879f7273ffc16e07fddd453e4e23aa
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-07 13:06:05 +0100

    Displaying Spinner While sendign request

--- src/assets/interface/burgetState.ts
+++ src/assets/interface/burgetState.ts
@@ -1,14 +1,15 @@
 interface burgerState {
-    ingredients: {
-        [propName: string]: number,
-        // salad: number,
-        // bacon: number,
-        // cheese: number,
-        // meat: number
-    },
-    totalPrice: number,
-    purchaseable: boolean
-    purchasing: boolean
+  ingredients: {
+    [propName: string]: number;
+    // salad: number,
+    // bacon: number,
+    // cheese: number,
+    // meat: number
+  };
+  totalPrice: number;
+  purchaseable: boolean;
+  purchasing: boolean;
+  loading: boolean;
 }
 
 export default burgerState;
