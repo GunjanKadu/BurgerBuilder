80c3b24e41f0a799ad020c973572d402e9b01598
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-15 11:09:38 +0100

    Adding Routing to the projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/burgerbuilder';
 import Checkout from './containers/Checkout/checkout';
+import { Route, Switch } from 'react-router-dom';
+
 const App: React.FC = () => {
   return (
     <div className='App'>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Switch>
+          <Route path='/' exact component={BurgerBuilder} />
+          <Route path='/checkout' component={Checkout} />
+        </Switch>
       </Layout>
     </div>
   );
