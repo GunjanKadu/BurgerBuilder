ec557489654d021633ad3a397461e87463d8f9c6
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-15 18:47:51 +0100

    Submitting Orders and Passing data between pages

--- src/axios-orders.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://burgerbuilder-752ae.firebaseio.com/"
-});
-
-export default instance;
--- src/components/UI/Backdrop/backdrop.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './backdrop.css'
-
-const backdrop = (props: any) => (
-    props.show ? <div className="Backdrop" onClick={props.clicked}></div> : null
-)
-export default backdrop;
