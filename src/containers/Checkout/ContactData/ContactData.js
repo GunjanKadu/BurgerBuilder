cd7328894bb5d7afb35e4066a66db85e74337712
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 00:29:53 +0100

    Input based on js config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -88,30 +88,23 @@ class ContactData extends Component {
   };
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
     let form = (
       <form>
-        <Input elementType='...' elementConfig='...' value='...' />
-        <Input
-          inputtype='input'
-          className={classes.Input}
-          type='email'
-          name='email'
-          placeholder='Your Mail'
-        />
-        <Input
-          inputtype='input'
-          className={classes.Input}
-          type='text'
-          name='street'
-          placeholder='Street'
-        />
-        <Input
-          inputtype='input'
-          className={classes.Input}
-          type='text'
-          name='postal'
-          placeholder='Postal Code'
-        />
+        {formElementsArray.map(formELement => (
+          <Input
+            key={formELement.id}
+            elementType={formELement.config.elementType}
+            elementConfig={formELement.config.elementConfig}
+            value={formELement.config.value}
+          />
+        ))}
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
         </Button>
